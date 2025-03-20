64919609a754122c7624b786a1482442556dfbde
ChoiAnYong <dksdyd78@naver.com>
2025-03-20 13:33:42 +0900

    feat: 필수 문제 3 구현

--- ChapterTwoAssignment/ChapterTwoAssignment/main.swift
+++ ChapterTwoAssignment/ChapterTwoAssignment/main.swift
@@ -20,3 +20,13 @@ let result = two.myMap([1, 2, 3, 4, 5]) {
 }
 
 print(result)
+
+
+let three = EssentialThree()
+print(three.a([1,2,3,4,5]))
+print(three.b(["가", "나", "다", "라", "마"]))
+
+print(three.c([6,7,8,9,10]))
+print(three.c(["바", "사", "아", "자", "차"]))
+
+print(three.d([2,4,6,8,10,12,14]))
