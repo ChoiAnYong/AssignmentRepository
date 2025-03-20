1d4f68bdc7d6ec82593878f201669a55cb3fb3d1
ChoiAnYong <dksdyd78@naver.com>
2025-03-20 14:38:40 +0900

    feat: Engine 구현

---
+++ ChapterTwoAssignment/ChapterTwoAssignment/Challenge/Engine.swift
@@ -0,0 +1,16 @@
+//
+//  Engine.swift
+//  ChapterTwoAssignment
+//
+//  Created by 최안용 on 3/20/25.
+//
+
+import Foundation
+
+protocol EngineProtocol { }
+
+class Engine: EngineProtocol { }
+
+class ElectricEngine: EngineProtocol { }
+
+class HybridEngine: EngineProtocol { }
